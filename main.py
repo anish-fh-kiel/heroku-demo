e200ba5f405e529e913b63221525ab0bdb6118f3
anish-fh-kiel <156146818+anish-fh-kiel@users.noreply.github.com>
2024-01-14 15:48:02 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
-import time
+import time, os
+from dotenv import load_env
+import logging
 
+load_env()
+url = os.getenv('url')
+api_key = os.getenv('api_key')
 counter = 0
 while counter<=5:
 	print("I am working!")
+	print(url)
+	loggig.info(f"api-key = {api_key}")
+	
 	time.sleep(3)
